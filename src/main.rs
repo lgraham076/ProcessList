d739cd6915690c0ef429821ebfbe758117f0783e
lgraham <langstongraham@gmail.com>
2018-07-15 23:33:41 -0500

    Updated parser to only view numbered directories

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@
 extern crate glob;
 
 fn main() {
+    parse_proc();
+}
+
+fn parse_proc() {
     use glob::glob;
 
-    // Output all nodes in /proc
-    for entry in glob("/proc/*").expect("Unable to read directory") {
+    // Output all numbered nodes in /proc
+    for entry in glob("/proc/[0-9]*").expect("Unable to read directory") {
         match entry {
             Ok(path) => println!("{:?}", path.display()),
             Err(e) => println!("{:?}", e),
